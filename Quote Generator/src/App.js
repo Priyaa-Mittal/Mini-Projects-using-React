8fea7f9cdabe4773ac4c7d4c18b878eef7ed73dc
Priya mittal <106603738+Priyaa-Mittal@users.noreply.github.com>
2022-12-12 23:28:10 +0530

    Update App.js

--- Quote Generator/src/App.js
+++ Quote Generator/src/App.js
@@ -1,15 +1,11 @@
-// import ToDo from './To-do List/todo';
-// import '././To-do List/todo.css';
-// import Advice from './Advice/advice';
-// import './Advice/advice.css';
-import './Weather/weather.css';
-import Weather from './Weather/weather.js';
+import Advice from './advice';
+import './advice.css';
 function App() {
   return (
     <div>
-      {/* <ToDo /> */}
-      {/* <Advice /> */}
-      {/* <Weather /> */}
+     
+       <Advice /> 
+      
     </div>
 
   );
